776f865b69036f65d901e439c9924f72b0a00bf2
om2123 <omvakhare150@gmail.com>
2023-10-13 15:19:40 +0530

    lint error fixed

--- src/compo/Product.jsx
+++ src/compo/Product.jsx
@@ -21,8 +21,8 @@ function Product({ id, title, image, price, rating }) {
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10 growing-hover">
       <Link to={`/product/${id}`}>
-
-        <img src={image} className=" w-72 object-contain" />
+          
+        <img src={image} alt="anything" className=" w-72 object-contain" />
         <h4 className="my-3">{title}</h4>
         <div className="flex">
           {Array(rating)
